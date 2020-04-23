a14097d3b8826354534aee550bcd55c16b4cc79f
ebenz99 <ebenz99@gmail.com>
2020-04-23 14:54:25 -0400

    multiple calls

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("What do you want to inspect?");
+//        System.out.println("What do you want to inspect?");
         Scanner ip = new Scanner(System.in);
 //        String decision = ip.nextLine();
         String decision = "Whetstone";
         if (decision.equals("Whetstone")){
             Whetstone w = new Whetstone();
-            System.out.println("here");
             w.run();
         }
         return;
--- src/Whetstone.java
+++ src/Whetstone.java
@@ -7,42 +7,54 @@ public class Whetstone extends Experiment {
         super();
     }
 
+    public List<String> buildCommands(int iterations){
+        List<String> cmds = new ArrayList<String>();
+        cmds.add("/bin/sh");
+        cmds.add("-c");
+        cmds.add("gcc -o whetstone /home/ethan/IdeaProjects/BenfordsLaw/whetstone.c -lm -O2 && sudo -S perf stat ./whetstone " + iterations );
+        return cmds;
+    }
+
     @Override
     public void run() {
         try {
-            List<String> cmds = new ArrayList<String>();
-            cmds.add("/bin/sh");
-            cmds.add("-c");
-//            cmds.add("gcc -o whetstone /home/ethan/IdeaProjects/BenfordsLaw/whetstone.c -lm -O2;");
-            cmds.add("sudo -S perf stat ./whetstone 2000");
-//            System.out.println("compiled");
 
-            ProcessBuilder p = new ProcessBuilder(cmds);
-            p.directory(new File("/home/ethan/code/performance"));
+            List<String> times = new ArrayList<String>();
 
-            Process proc = p.start();
-            BufferedReader stdout = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            BufferedReader stderr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-            OutputStream stdin = proc.getOutputStream();
+            for (int j = 1000; j < 11000; j += 1000) {
+                List<String> cmds = buildCommands(j);
+
+                ProcessBuilder p = new ProcessBuilder(cmds);
+                p.directory(new File("/home/ethan/code/performance"));
+
+                Process proc = p.start();
+                BufferedReader stdout = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                BufferedReader stderr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+                OutputStream stdin = proc.getOutputStream();
 
 
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-            writer.write((new Login()).getPassword());
-            writer.flush();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+                writer.write((new Login()).getPassword());
+                writer.flush();
 
-            List<String> op = new ArrayList<String>();
-            String line;
-            while ((line = stderr.readLine()) != null) {
-                op.add(line.trim());
-            }
-            stdout.close();
-            stdin.close();
-            stderr.close();
+                List<String> op = new ArrayList<String>();
+                String line;
 
-            for (int i = 0; i < op.size(); i++) {
-                if (op.get(i).contains("seconds time")) {
-                    String seconds = op.get(i).substring(0, op.get(i).indexOf(' ')).substring(4);
-                    System.out.println(seconds);
+
+                while ((line = stderr.readLine()) != null) {
+                    op.add(line.trim());
+                }
+                stdout.close();
+                stdin.close();
+                stderr.close();
+
+                for (int i = 0; i < op.size(); i++) {
+                    if (op.get(i).contains("seconds time")) {
+//                    String seconds = op.get(i).substring(0, op.get(i).indexOf(' ')).substring(4);
+//                    System.out.println(op.get(i));
+                        String seconds = op.get(i).replace(".", "");
+                        System.out.println(seconds);
+                    }
                 }
             }
         } catch (IOException e) {
