63699096c7605b3c3ff4e69492fbc35fd1433c18
ebenz99 <ebenz99@gmail.com>
2020-04-23 15:58:34 -0400

    getting interesting results

--- src/Whetstone.java
+++ src/Whetstone.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.Character.forDigit;
 
 public class Whetstone extends Experiment {
     public Whetstone(){
@@ -15,6 +18,34 @@ public class Whetstone extends Experiment {
         return cmds;
     }
 
+    public void submitSudoPassword(OutputStream stdin) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+        writer.write((new Login()).getPassword());
+        writer.flush();
+    }
+
+    public List<String> readTerminalOutput(BufferedReader std) throws IOException {
+        List<String> op = new ArrayList<String>();
+        String line;
+        while ((line = std.readLine()) != null) {
+            op.add(line.trim());
+        }
+        return op;
+    }
+
+    public void processNumbers(List<String> times){
+        int[] occurrences = new int[10];
+
+        for(String time : times){
+            System.out.println(time);
+            for(char x : time.toCharArray()){
+                System.out.println(((int) x) - 48);
+                occurrences[((int) x) - 48] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(occurrences));
+    }
+
     @Override
     public void run() {
         try {
@@ -26,37 +57,31 @@ public class Whetstone extends Experiment {
 
                 ProcessBuilder p = new ProcessBuilder(cmds);
                 p.directory(new File("/home/ethan/code/performance"));
-
                 Process proc = p.start();
+
                 BufferedReader stdout = new BufferedReader(new InputStreamReader(proc.getInputStream()));
                 BufferedReader stderr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
                 OutputStream stdin = proc.getOutputStream();
 
+                submitSudoPassword(stdin);
 
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-                writer.write((new Login()).getPassword());
-                writer.flush();
+                List<String> op = readTerminalOutput(stderr);
 
-                List<String> op = new ArrayList<String>();
-                String line;
-
-
-                while ((line = stderr.readLine()) != null) {
-                    op.add(line.trim());
-                }
                 stdout.close();
                 stdin.close();
                 stderr.close();
 
                 for (int i = 0; i < op.size(); i++) {
                     if (op.get(i).contains("seconds time")) {
-//                    String seconds = op.get(i).substring(0, op.get(i).indexOf(' ')).substring(4);
-//                    System.out.println(op.get(i));
-                        String seconds = op.get(i).replace(".", "");
-                        System.out.println(seconds);
+                        String seconds = op.get(i).replaceAll("\\D", "");
+                        times.add(seconds);
                     }
                 }
             }
+
+            processNumbers(times);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
